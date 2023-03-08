f7a35b12a1f35764e98c5fbf4f3b4fc963b49c8f
MunVlad <mvv2006@ukr.net>
2023-03-08 18:57:22 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,67 @@
+import player from "@vimeo/player";
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+ const onPlay = function({duration, percent, seconds}) {
+    {
+    duration: 61.857
+    percent: 0.049
+    seconds: 3.034
+}
+};
+
+player.on('timeupdate', onPlay);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function onStart({ seconds }) {
+//   localStorage.setItem(currentTime, seconds);
+// }
+// player.setCurrentTime(localStorage.getItem(currentTime));
