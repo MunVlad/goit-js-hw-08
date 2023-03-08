cb21f0e1ed5b783bc13fafb61d40199471c8136e
MunVlad <mvv2006@ukr.net>
2023-03-08 22:06:14 +0200

    2-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,18 @@
-import player from "@vimeo/player";
+import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-const currentTime = "videoplayer-current-time";
+const CURRENT_TIME = "videoplayer-current-time";
 
- const onPlay = function({seconds}) {
+ const onPlay = ({seconds}) => {
   
-   localStorage.setItem(currentTime, seconds);
+   localStorage.setItem(CURRENT_TIME, seconds);
  };
+
+const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlay, 1000));
-player.setCurrentTime(localStorage.getItem(currentTime));
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
+
 
 
 
