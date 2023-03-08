892d64680ad04b9760f5b29bf91fdc90feb6100e
MunVlad <mvv2006@ukr.net>
2023-03-08 19:41:06 +0200

    2-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,17 +3,14 @@ import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+const currentTime = "videoplayer-current-time";
 
  const onPlay = function({seconds}) {
   
-
-
+   localStorage.setItem(currentTime, seconds);
  };
-localStorage.setItem("videoplayer-current-time", 'fsdfdf'
-);
-console.log(localStorage);
 player.on('timeupdate', throttle(onPlay, 1000));
-
+player.setCurrentTime(localStorage.getItem(currentTime));
 
 
 
