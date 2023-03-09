7c78578607067fd69b1c870241b3dad3c262c10e
MunVlad <mvv2006@ukr.net>
2023-03-09 11:52:20 +0200

    3-task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+const input = document.querySelector('input');
 
+form.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('input', onTextareaText);
+input.addEventListener('input', onInputText);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const formData = new FormData(evt.currentTarget);
+  console.log(formData);
+}
+
+function onTextareaText(evt) {
+  const message = evt.currentTarget.value;
+ 
+}
+
+function onInputText(evt) {
+  const email = evt.currentTarget.value;
+
+}
